d370447e5ca3265d18f2175977a72863e6a49685
dfazhra <h.difaazzahra@gmail.com>
2025-06-24 17:22:55 +0700

    Add files via upload

---
+++ Hanari/models/muffin.py
@@ -0,0 +1,16 @@
+from produk import Produk
+
+class Muffin(Produk):
+    def tampilkan_info(self):
+        print(f"\n[{self.kode}] Muffin")
+        for bahan, jumlah in self.bahan_baku.items():
+            print(f"    - {bahan}: {jumlah}")
+        print(f"  Biaya Produksi (per {self.jumlah_n} pcs): Rp{self.biaya_produksi}")
+        print(f"  Harga Jual      (per {self.jumlah_n} pcs): Rp{self.harga_jual}")
+
+    def simulasi_produksi(self):
+        print("\nProduksi Muffin:")
+        print("1. Pengadonan")
+        print("2. Pengembangan (fermentasi)")
+        print("3. Pemanggangan")
+        print("4. Penambahan topping")
