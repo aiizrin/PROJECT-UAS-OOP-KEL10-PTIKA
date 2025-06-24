74b9ecd96abf7b846a52f7b00a7a05fc6b6299ab
khillatr <akhillatjandra@gmail.com>
2025-06-24 17:23:45 +0700

    create produk

---
+++ Hanari/models/produk.py
@@ -0,0 +1,19 @@
+from abc import ABC, abstractmethod
+
+
+class Produk(ABC):
+    def __init__(self, kode, nama, bahan_baku, biaya_produksi, harga_jual, jumlah_n):
+        self.kode = kode
+        self.nama = nama
+        self.bahan_baku = bahan_baku
+        self.biaya_produksi = biaya_produksi
+        self.harga_jual = harga_jual
+        self.jumlah_n = jumlah_n
+
+    @abstractmethod
+    def tampilkan_info(self):
+        pass
+
+    @abstractmethod
+    def simulasi_produksi(self):
+        pass
